f2845ce1d8cf4e1c834ad5f842d6c7e9b899c1ce
nhutran <tknhu130@gmail.com>
2024-10-05 00:04:30 +0700

    update controller and service in api

--- ApiWithSolid.Presentation/Controllers/ProductController.cs
+++ ApiWithSolid.Presentation/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using ApiWithSolid.Application.UsecaseInterface;
+﻿using ApiWithSolid.Application.ProductDTOs;
+using ApiWithSolid.Application.UsecaseInterface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ApiWithSolid.Presentation.Controllers
 {
@@ -9,6 +11,52 @@ namespace ApiWithSolid.Presentation.Controllers
     public class ProductController(IProductService productService): ControllerBase
     {
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct()
+        {
+            var products = await productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> GetById(int productId)
+        {
+            var product = await productService.GetProductByIdAsync(productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto createProductDto)
+        {
+            await productService.AddProductAsync(createProductDto);
+            return CreatedAtAction(nameof(GetById), new { id = createProductDto.Name }, createProductDto);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] UpdateProductDto updateProductDto)
+        {
+            if (id != updateProductDto.Id)
+            {
+                return BadRequest();
+            }
+            await productService.UpdateProductAsync(updateProductDto);
+            return Ok(updateProductDto);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await productService.DeleteProductAsync(id);
+            return Ok("Delete Successfully"); 
+        }
 
     }
 }
