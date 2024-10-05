45d80c185a6b39b7223adcdf64ebf423fd35cf9b
nhutran <tknhu130@gmail.com>
2024-10-05 14:14:13 +0700

    delete comment not use

--- ApiWithSolid.Presentation/Program.cs
+++ ApiWithSolid.Presentation/Program.cs
@@ -3,10 +3,8 @@ using ApiWithSolid.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
@@ -14,7 +12,6 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
